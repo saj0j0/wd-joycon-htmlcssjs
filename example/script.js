b8cf710f5d0b8536a96511d4b43b4de8118dc7f8
saj0j0 <gjefdee@gmail.com>
2023-06-15 20:37:25 +0700

    c2

---
+++ example/script.js
@@ -0,0 +1,11 @@
+var btnu = document.getElementsByClassName("btup")
+var btnl = document.getElementsByClassName("btleft")
+var btd = document.getElementsByClassName("btdown")
+var btnr = document.getElementsByClassName("btright")
+
+
+if (btnu = true) {
+    console.log(btnu)
+} else if (btnl = true) {
+    console.log(btnu)
+}
